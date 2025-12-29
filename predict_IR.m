f6bd6fe4b07b6eb33d40ca44e9eaa4c3e09a2356
rb505 <140686552+ivpml84079@users.noreply.github.com>
2025-12-29 18:05:08 +0800

    Delete predict_IR.m

--- predict_IR.m
+++
@@ -1,8 +0,0 @@
-function [IR] = predict_IR(rad_diff, scale_ratio)
-
-first_filter = histogram_filter(rad_diff, 0.1, "angel", []);
-
-scale_ratio = scale_ratio(first_filter);
-second_filter = histogram_filter(scale_ratio, 0.5, "length", first_filter);
-
-IR  = size(second_filter, 1) / size(rad_diff, 1);
